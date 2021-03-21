2fdf2666859abb8e34263831b30bd20a2b7d825b
AntXpras <80271241+AntXpras@users.noreply.github.com>
2021-03-21 09:00:37 +0700

    Update

--- handlers/admins.py
+++ handlers/admins.py
@@ -22,25 +22,25 @@ async def pause(_, message: Message):
     ) or (
             callsmusic.pytgcalls.active_calls[message.chat.id] == 'paused'
     ):
-        await message.reply_text(f"**{BN} :**❕ Nothing is playing.")
+        await message.reply_text(f"**{BN} :**❕ Ohh FUCK Nothing is playing.")
     else:
         callsmusic.pytgcalls.pause_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :** ⏸ Paused.")
+        await message.reply_text(f"**{BN} :** ⏸The Fucking Human Paused music.")
 
 
-@Client.on_message(command(["resume", "r"]))
+@Client.on_message(command(["resume anying", "r"]))
 @errors
 @admins_only
 async def resume(_, message: Message):
     if (
             message.chat.id not in callsmusic.pytgcalls.active_calls
     ) or (
-            callsmusic.pytgcalls.active_calls[message.chat.id] == 'playing'
+            callsmusic.pytgcalls.active_calls[message.chat.id] == 'Lanjottttttttt'
     ):
-        await message.reply_text(f"**{BN} :**❕ Nothing is paused.")
+        await message.reply_text(f"**{BN} :**❕ WOI ANYING KALO GAMAU DENGERIN MUSIK JANGAN LUPA MATIIN ISH KAU NIII.")
     else:
         callsmusic.pytgcalls.resume_stream(message.chat.id)
-        await message.reply_text(f"**{BN} :** ▶️ Resumed.")
+        await message.reply_text(f"**{BN} :** ▶▶▶KIAMAT DITUNDA.")
 
 
 @Client.on_message(command(["stop", "s"]))
@@ -48,7 +48,7 @@ async def resume(_, message: Message):
 @admins_only
 async def stop(_, message: Message):
     if message.chat.id not in callsmusic.pytgcalls.active_calls:
-        await message.reply_text(f"**{BN} :**❕ Nothing is streaming.")
+        await message.reply_text(f"**{BN} :**❕ ANJIR GADA YANG STREAMING NIH.")
     else:
         try:
             queues.clear(message.chat.id)
