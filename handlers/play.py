396cfcce46874b2c93e1a3ee54dab3a39cc542db
AntXpras <80271241+AntXpras@users.noreply.github.com>
2021-03-21 09:06:38 +0700

    Update

--- handlers/play.py
+++ handlers/play.py
@@ -56,7 +56,7 @@ async def play(_, message: Message):
                         break
 
         if offset in (None,):
-            await res.edit_text(f"**{Bn} :**❕ You did not give me anything to play.")
+            await res.edit_text(f"**{Bn} :**❕ WOI BANGKE LINK NYA MANA, LINK WOI LINK , BUKAN LINK BOKEP.")
             return
 
         url = text[offset:offset + length]
@@ -67,5 +67,9 @@ async def play(_, message: Message):
         position = queues.add(message.chat.id, file_path)
         await res.edit_text(f"**{Bn} :** #️⃣ Queued at position {position}.")
     else:
-        await res.edit_text(f"**{Bn} :** ▶️ Playing...")
+        await res.edit_text(f"**{Bn} :** ▶️ ANJAI GELENG-GELENG LAGI KITA.....")
+                            
+                            
+                            
+                          ...")
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path, 48000, callsmusic.pytgcalls.get_cache_peer())
